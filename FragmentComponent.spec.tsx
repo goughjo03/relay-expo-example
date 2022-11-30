fdf11a219d00b4667728b57c4ad385835be6f2c1
Joey Gough <j.gough@novafutur.com>
2022-11-30 00:38:31 +0000

    add testing

---
+++ FragmentComponent.spec.tsx
@@ -0,0 +1,84 @@
+import {
+  graphql,
+  RelayEnvironmentProvider,
+  useLazyLoadQuery,
+} from "react-relay";
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils";
+import FragmentComponent from "./FragmentComponent";
+import renderer, { act } from "react-test-renderer";
+import { Suspense } from "react";
+
+describe("FragmentComponent", () => {
+  it("renders correctly with automatically generated data", async () => {
+    const environment = createMockEnvironment();
+    const TestRenderer = () => {
+      const data = useLazyLoadQuery(
+        graphql`
+          query FragmentComponentQuery @relay_test_operation {
+            myData: country(code: "test-id") {
+              # Spread the fragment you want to test here
+              ...FragmentComponent_country
+            }
+          }
+        `,
+        {}
+      );
+      return <FragmentComponent country={(data as any).myData} />;
+    };
+    const tree = renderer.create(
+      <RelayEnvironmentProvider environment={environment}>
+        <Suspense fallback="Loading...">
+          <TestRenderer />
+        </Suspense>
+      </RelayEnvironmentProvider>
+    );
+    act(() => {
+      environment.mock.resolveMostRecentOperation((operation) =>
+        MockPayloadGenerator.generate(operation)
+      );
+    });
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it("renders correctly with specifically generated data", async () => {
+    const environment = createMockEnvironment();
+    const TestRenderer = () => {
+      const data = useLazyLoadQuery(
+        graphql`
+          query FragmentComponentQuery @relay_test_operation {
+            myData: country(code: "test-id") {
+              # Spread the fragment you want to test here
+              ...FragmentComponent_country
+            }
+          }
+        `,
+        {}
+      );
+      return <FragmentComponent country={(data as any).myData} />;
+    };
+    const tree = renderer.create(
+      <RelayEnvironmentProvider environment={environment}>
+        <Suspense fallback="Loading...">
+          <TestRenderer />
+        </Suspense>
+      </RelayEnvironmentProvider>
+    );
+    act(() => {
+      environment.mock.resolveMostRecentOperation((operation) =>
+        MockPayloadGenerator.generate(operation, {
+          Country() {
+            return {
+              phone: "123456789",
+              currency: "USD",
+              continent: {
+                name: "Europe",
+              },
+            };
+          },
+        })
+      );
+    });
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ __generated__/FragmentComponentQuery.graphql.ts
@@ -0,0 +1,152 @@
+/**
+ * @generated SignedSource<<668acb976035b279632b3e90dad6228e>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type FragmentComponentQuery$variables = {};
+export type FragmentComponentQuery$data = {
+  readonly myData: {
+    readonly " $fragmentSpreads": FragmentRefs<"FragmentComponent_country">;
+  } | null;
+};
+export type FragmentComponentQuery = {
+  response: FragmentComponentQuery$data;
+  variables: FragmentComponentQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "code",
+    "value": "test-id"
+  }
+],
+v1 = {
+  "enumValues": null,
+  "nullable": false,
+  "plural": false,
+  "type": "String"
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "FragmentComponentQuery",
+    "selections": [
+      {
+        "alias": "myData",
+        "args": (v0/*: any*/),
+        "concreteType": "Country",
+        "kind": "LinkedField",
+        "name": "country",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "FragmentComponent_country"
+          }
+        ],
+        "storageKey": "country(code:\"test-id\")"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "FragmentComponentQuery",
+    "selections": [
+      {
+        "alias": "myData",
+        "args": (v0/*: any*/),
+        "concreteType": "Country",
+        "kind": "LinkedField",
+        "name": "country",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "phone",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "currency",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Continent",
+            "kind": "LinkedField",
+            "name": "continent",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "name",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "country(code:\"test-id\")"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "c5eb0f66def4e6cefaa13c17b54ff298",
+    "id": null,
+    "metadata": {
+      "relayTestingSelectionTypeInfo": {
+        "myData": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Country"
+        },
+        "myData.continent": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "Continent"
+        },
+        "myData.continent.name": (v1/*: any*/),
+        "myData.currency": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "String"
+        },
+        "myData.phone": (v1/*: any*/)
+      }
+    },
+    "name": "FragmentComponentQuery",
+    "operationKind": "query",
+    "text": "query FragmentComponentQuery {\n  myData: country(code: \"test-id\") {\n    ...FragmentComponent_country\n  }\n}\n\nfragment FragmentComponent_country on Country {\n  phone\n  currency\n  continent {\n    name\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "dce66be991711c44a30196ef3dbc774d";
+
+export default node;
